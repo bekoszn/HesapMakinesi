7c815daa7a605f97baaa81de6a18777f111b1a81
bekoszn <berkeozguder@gmail.com>
2024-09-24 11:13:57 +0300

    Initial Commit

---
+++ HesapMakinesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HesapMakinesi
+//
+//  Created by Berke Özgüder on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
